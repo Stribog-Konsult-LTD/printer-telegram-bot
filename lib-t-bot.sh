3de7c31d72d7f07b7defde931946c336b62e93ae
nicknesh <31367667+nicknesh@users.noreply.github.com>
2022-05-14 23:31:04 +0300

    Initial release

---
+++ lib-t-bot.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+
+BASEDIR=$( readlink -f $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) )
+
+printred(){
+    RED='\033[06;33m'
+    NC='\033[00m' # No Color
+    echo -e "\n${RED}${@}${NC} \n"
+}
+
+printgreen(){
+    GREEN='\033[06;32m'
+    NC='\033[00m' # No Color
+    echo -e  "\n${GREEN}${@}${NC} \n"
+}
+
+printblue(){
+    GREEN='\033[01;34m'
+    NC='\033[00m' # No Color
+    echo -e  "\n${GREEN}${@}${NC} \n"
+}
+
+
+
+getConfigItem() {
+    local section="$1"
+    local key="$2"
+    echo $(crudini --get  "${CONFIG_FILE_NAME}" "${section}" "${key}" )
+}
+
+setConfigItem(){
+    local section="$1"
+    local key="$2"
+    local value="$3"
+    crudini --set  "${CONFIG_FILE_NAME}" "${section}" "${key}"  "${value}"
+}
+
+T_BOT_KEY=$(getConfigItem "general" "botKey" )
+if [ -z "$T_BOT_KEY" ] ; then
+    printred "Please add botKey in $CONFIG_FILE_NAME [general]"
+    setConfigItem "general" "botKey" "Bot key here"
+    exit 1
+fi
+
+T_API="https://api.telegram.org/bot$T_BOT_KEY"
+T_FILE="https://api.telegram.org/file/bot$T_BOT_KEY"
+TELEGRAM_CURL="timeout -s KILL 4 curl -k -s --max-time 10 --connect-timeout 3 "
+
+
+getLastKnown(){
+    echo "$(getConfigItem "general" "lastKnownMessageId")"
+}
+
+setLastKnown(){
+    local lastKnown="$1"
+    setConfigItem "general" "lastKnownMessageId" "$lastKnown"
+}
+
+
+getUpdates(){
+    local lastKnown=$(getLastKnown)
+    local jsonResult=$( 
+    $TELEGRAM_CURL \
+    -X GET \
+    ${T_API}/getUpdates"?limit=1&offset=${lastKnown}&timeout=10" )
+    
+    echo ${jsonResult}
+}
+
+isNumber(){
+    local re='^[0-9]+$'
+    local number="$1"
+    if ! [[ $number =~ $re ]] ; then
+        echo "no"
+    else
+        echo "yes"
+    fi
+}
+
+getJsonValue(){
+    local json=$1
+    local key=$2
+    value=$(echo "$json" | jq -r "$key // empty")
+    echo $value
+
+}
---
+++ printer_bot.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+
+BASEDIR=$( readlink -f $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) )
+pushd $BASEDIR
+
+CONFIG_FILE_NAME="${BASEDIR}/config.ini"
+
+source "./lib-t-bot.sh"
+
+# Generate config file if not exists
+[ -z "$(crudini --get "$CONFIG_FILE_NAME" general lastKnownMessageId)" ] \
+    && $(crudini --set "$CONFIG_FILE_NAME" general lastKnownMessageId 1)
+
+DOWNLOAD_DIR="${BASEDIR}/downloads"
+# Make download dir if not exists
+mkdir -p "$DOWNLOAD_DIR"
+
+
+
+PRINT_MASTER=$(getConfigItem "general" "master" )
+if [ -z "$PRINT_MASTER" ] ; then
+    printred "Please add master chat id in $CONFIG_FILE_NAME [general]"
+    setConfigItem "general" "master" "Master chat id here"
+
+    exit 1
+fi
+
+
+
+
+
+processFile(){
+    local file_name=$1
+    local file_id=$2
+    local mime_type=$3
+    local caption=$4
+    printgreen "processFile file_name:$file_name, caption: $caption, file_id: $file_id, mime_type: $mime_type"
+
+    local fileInfo=$($TELEGRAM_CURL \
+    -X GET \
+    ${T_API}/getFile"?file_id=${file_id}" )
+
+    if [ "$(getJsonValue "$fileInfo" ".ok" )" == true ] ; then
+        local file_path="$(getJsonValue "$fileInfo" ".result.file_path")"
+        download_file_name="$DOWNLOAD_DIR/$file_name"
+        wget ${T_FILE}/$file_path -O "$download_file_name"
+        if [ "${mime_type}" == "application/pdf" ] ; then
+            lp_output=$(lp  "$download_file_name")
+            echo "lp_output: $lp_output"
+        fi
+    else
+        printred "Error fileInfo"
+        echo "$fileInfo" | jq
+    fi
+
+
+}
+
+
+# processText(){
+# }
+#
+# processCommand(){
+# }
+
+processMessage(){
+    local jsonResult="$1"
+    if [ "$(getJsonValue "$jsonResult" ".ok" )" == "true" ] ; then
+        echo $jsonResult | jq
+        local update_id="$(getJsonValue "$jsonResult" '.result[0].update_id' )"
+        if [ "$(isNumber $update_id)" == 'yes' ] ; then
+            setLastKnown $( expr $update_id + 1)
+        else
+            printred "Unknown  update_id:  $update_id"
+            return
+        fi
+        local chatId="$(getJsonValue "$jsonResult" '.result[0].message.chat.id')"
+        if [ "$chatId" == "$PRINT_MASTER" ] ; then
+            local file_name="$(getJsonValue "$jsonResult" '.result[0].message.document.file_name' )"
+            if [  -n "$file_name" ] ; then
+                printgreen "It is file: $file_name"
+                local caption="$(getJsonValue "$jsonResult" '.result[0].message.caption' )"
+                echo "File name is: $file_name, caption: $caption"
+                processFile \
+                    "$file_name"  \
+                    "$(getJsonValue "$jsonResult" '.result[0].message.document.file_id' )" \
+                    "$(getJsonValue "$jsonResult" '.result[0].message.document.mime_type' )" \
+                    "$caption"
+            else
+                local text="$(getJsonValue "$jsonResult" '.result[0].message.text' )"
+                echo "text: $text"
+                echo "$jsonResult" | jq
+            fi
+        else
+            printred "Wrong chat: $chatId / $PRINT_MASTER"
+        fi
+    else
+        printblue "No new messages"
+
+    fi
+
+}
+
+
+while [ true ]
+do
+    jsonResult=$(getUpdates)
+    processMessage "$jsonResult"
+done
+
+
+popd
